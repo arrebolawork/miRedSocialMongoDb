592e10f5317508dee48c7f3d7f13813f721823c0
David Arrebola <arrebola.work@gmail.com>
2025-06-21 10:34:46 +0200

    cambios

--- config/config.js
+++ config/config.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
+console.log("MONGO_URI:", process.env.MONGO_URI);
 const dbConnection = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("❌ MONGO_URI no está definida");
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Base de datos conectada con éxito");
+    console.log("✅ Base de datos conectada con éxito");
   } catch (error) {
-    console.error(error);
+    console.error("🛑 Error:", error);
     throw new Error("Error a la hora de iniciar la base de datos");
   }
 };
