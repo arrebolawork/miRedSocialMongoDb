1cfbfd215a32217cfc118e7755c3256be5273fc9
David Arrebola <arrebola.work@gmail.com>
2025-08-02 21:30:34 +0200

    modificado el middleware

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,17 +4,27 @@ require("dotenv").config();
 
 const authentication = async (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).send({ message: "No token proporcionado" });
+    }
+
+    const token = authHeader.replace(/^Bearer\s/, "");
+
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ _id: payload._id, tokens: token });
+
+    const user = await User.findById(payload._id);
+
     if (!user) {
       return res.status(401).send({ message: "No estás autorizado" });
     }
+
     req.user = user;
     next();
   } catch (error) {
     console.error(error);
-    return res.status(500).send({ error, message: "Ha habido un problema con el token" });
+    return res.status(401).send({ error, message: "Ha habido un problema con el token" });
   }
 };
 module.exports = { authentication };
