002104dad7901d2473ce80449f15aea47f965c9e
David Arrebola <arrebola.work@gmail.com>
2025-08-09 15:32:35 +0200

    creando carpeta upload

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 const { dbConnection } = require("./config/config");
 const path = require("path");
 const cors = require("cors");
+const uploadsPath = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsPath)) {
+  fs.mkdirSync(uploadsPath);
+}
 app.use(
   cors({
     origin: "http://localhost:5173", // O reemplaza con tu dominio, ej: 'https://tufrontend.com'
