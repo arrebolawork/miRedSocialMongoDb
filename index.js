60d81096eac0adf010247eecbcee51a8bd29064a
David Arrebola <arrebola.work@gmail.com>
2025-09-05 20:01:59 +0200

    añadido para despliegue

--- index.js
+++ index.js
@@ -12,13 +12,17 @@ if (!fs.existsSync(uploadsPath)) {
 }
 app.use(
   cors({
-    origin: "http://localhost:5173", // O reemplaza con tu dominio, ej: 'https://tufrontend.com'
+    origin: process.env.FRONTEND_URL || "*", // O reemplaza con tu dominio, ej: 'https://tufrontend.com'aqui tenia el localhosr
   })
 );
 app.use(express.json());
 dbConnection();
-
+const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/users", require("./routes/user"));
 app.use("/posts", require("./routes/post"));
+app.use(express.static(path.join(__dirname, "public", "dist"))); //esto no estaba
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "dist", "index.html"));
+});
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
