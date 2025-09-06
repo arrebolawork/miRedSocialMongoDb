7042dfccc9b0a3b47b4b1ee65ae8139f0c924b80
David Arrebola <arrebola.work@gmail.com>
2025-09-06 13:13:10 +0200

    postController

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+// Debería ser algo como:
 const PostController = require("../controllers/PostController");
+// No: postController
 const ComentarioController = require("../controllers/ComentarioController");
 const { authentication } = require("../middlewares/authentication");
 const upload = require("../middlewares/upload");
@@ -9,7 +11,7 @@ router.get("/allPosts", PostController.getAllPosts);
 router.get("/id/:_id", PostController.getPostById);
 router.get("/titulo/:titulo", PostController.getPostByTitulo);
 router.get("/user", authentication, PostController.getPostsByUser);
-router.post("/create", upload.single("postImage"), authentication, postController.create);
+router.post("/create", upload.single("postImage"), authentication, PostController.create);
 router.post("/comentario/:postId", authentication, ComentarioController.create);
 router.put("/id/:_id", authentication, PostController.update);
 router.post("/like/:_id", authentication, PostController.like);
